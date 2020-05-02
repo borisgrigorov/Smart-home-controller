f124b6cab5783de6546e04fa2456f5a84c92aa60
Boris Grigorov <boris.grigorov26@gmail.com>
2020-05-02 12:19:47 +0200

    Add python script

---
+++ Ir_codes_writer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import io
+text = "";
+ffile = open("done.txt", "a")
+with open('ir.txt', 'r', encoding="utf-8") as file:
+    for raw in file:
+        ffile.write("delayMicroseconds(" + raw.split(' ')[0] + ");\n");
+        print(raw.split(' '))
+        ffile.write("pulseIR(" + raw.split(' ')[2] + ");\n");
