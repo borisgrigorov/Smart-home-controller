aad2e8effb8f64c076601bab01f0652d6323a938
Boris Grigorov <boris.grigorov26@gmail.com>
2020-05-16 17:42:00 +0200

    -

--- Ir_codes_writer.py
+++ Ir_codes_writer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from pathlib import Path
 import io
-text = ""In *Smart_Home_Controller.ino* file in callback function set this function on the message you want to.
 ffile = open("done.txt", "a")
 with open('ir.txt', 'r', encoding="utf-8") as file:
     for raw in file:
